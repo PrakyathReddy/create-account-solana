36937a6f7b979fc82839c25cf46f176477d7077b
PrakyathReddy <prakyathreddyk@gmail.com>
2023-09-11 09:39:53 +0530

    built and tested successfully

--- programs/create-account/src/lib.rs
+++ programs/create-account/src/lib.rs
@@ -1,15 +1,42 @@
 use anchor_lang::prelude::*;
+use anchor_lang::system_program;
 
 declare_id!("82qMKny4FNb1ZXiwD9xQEXEV7bjEeUCZcrmjMMizho1M");
 
+const LAMPORTS_PER_SOL: u64 = 1_000_000_000;
+
 #[program]
-pub mod create_account {
+pub mod create_system_account {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_system_account(ctx: Context<CreateSystemAccount>) -> Result<()> {
+        msg!("Program invoked, creating a system account....");
+        let new_pubkey = ctx.accounts.new_account.key().to_string();
+        msg!("New public key will be: {}", new_pubkey);
+
+        system_program::create_account(
+            CpiContext::new(
+                ctx.accounts.system_program.to_account_info(),
+                system_program::CreateAccount {
+                    from: ctx.accounts.payer.to_account_info(),
+                    to: ctx.accounts.new_account.to_account_info(),
+                },
+            ),
+            LAMPORTS_PER_SOL,                   // 1 SOL
+            0,                                  // space = 0 bytes
+            &ctx.accounts.system_program.key(), // program id
+        )?;
+
+        msg!("Account created successfully!");
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateSystemAccount<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(mut)]
+    pub new_account: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
